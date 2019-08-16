793aaace6386e603ff024b08cbb6ae33e266ee19
Tim Dohmen <dohmentim@yahoo.com>
2019-08-16 14:11:03 -0600

    buttons working

---
+++ game.js
@@ -0,0 +1,93 @@
+
+let healthEle = document.getElementById("health")
+let hitEle = document.getElementById("hit-counter")
+let restartElem = document.getElementById("restart")
+// let moves = {
+//   slap: 1,
+//   kick: 5,
+//   punch: 10
+// }
+let player1 = {
+  name: "Guy",
+  health: 100,
+  moves: {
+    slap: 1,
+    kick: 5,
+    punch: 10
+  },
+  inventory: []
+
+
+}
+
+let items = {
+  pGun: {
+    name: "Portal Gun",
+    modifier: 5,
+    description: "Opens a portal to antother dimension",
+  },
+  mBox: {
+    name: "Mr.Meeseeks Box",
+    modifier: 10,
+    description: "Summon multiple Meeseeks for assistance",
+
+  },
+  mSeed: {
+    name: "Mega Seeds!",
+    modifier: 15,
+    description: "You know where to put these....",
+  },
+}
+let timesHit = 0
+let multiplier = items.modifier
+
+// function giveItem(item) {
+//   player1.inventory.push(item)
+// }
+
+function addMods(stuff) {
+  for (let i = 0; i <= items.length; i++) {
+    if (stuff == multiplier) {
+    } player1.health -= multiplier
+  }
+}
+
+
+
+function hit(style) {
+  // if (style == "slap") {
+  //   health -= 1
+  // } else if (style == "kick") {
+  //   health -= 5
+  // } else if (style == "punch") {
+  //   health -= 10
+  // }
+  let pow = player1.moves[style]
+
+  if (player1.health > 0) {
+    player1.health -= pow
+
+  } else {
+    restartElem.classList.remove('hidden')
+    restartElem.innerText = "You beat Raid Boss Morty and find Ricks portal gun lying in the corner"
+  }
+
+  draw()
+}
+
+function win() {
+
+}
+function replay() {
+  player1.health = 100;
+  restartElem.classList.add('hidden')
+}
+
+function draw() {
+  healthEle.innerText = player1.health.toString()
+  hitEle.innerText = `${timesHit++}`
+}
+draw()
+
+
+
